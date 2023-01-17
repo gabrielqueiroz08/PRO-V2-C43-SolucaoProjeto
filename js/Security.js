765e2f33f8b3fba020f42f076b5c54c0e6f3ed64
gabrielqueiroz08 <100386457+gabrielqueiroz08@users.noreply.github.com>
2023-01-17 18:25:28 -0300

    awdawdwda
    wad

--- js/Security.js
+++ js/Security.js
@@ -1,36 +1,36 @@
 class Security {
 
-    constructor(){
+    constructor() {
 
         this.access1 = createInput("Código 1")
-        this.access1.position(100,90);
-        this.access1.style('background', 'white');  
+        this.access1.position(100, 90);
+        this.access1.style('background', 'white');
 
         this.button1 = createButton('Verificar');
-        this.button1.position(100,120);
-        this.button1.style('background', 'lightgrey');    
+        this.button1.position(100, 120);
+        this.button1.style('background', 'lightgrey');
 
         this.access2 = createInput("Código 1")
-        this.access2.position(700,190);
-        this.access2.style('background', 'white');  
+        this.access2.position(700, 190);
+        this.access2.style('background', 'white');
 
         this.button2 = createButton('Verificar');
-        this.button2.position(700,220);
+        this.button2.position(700, 220);
         this.button2.style('background', 'lightgrey');
 
         this.access3 = createInput("Código 1")
-        this.access3.position(100,290);
-        this.access3.style('background', 'white');  
+        this.access3.position(100, 290);
+        this.access3.style('background', 'white');
 
         this.button3 = createButton('Verificar');
-        this.button3.position(100,320);
-        this.button3.style('background', 'lightgrey'); 
+        this.button3.position(100, 320);
+        this.button3.style('background', 'lightgrey');
     }
 
-    display(){
+    display() {
 
         this.button1.mousePressed(() => {
-            if(system.authenticate(accessCode1,this.access1.value())){
+            if (system.authenticate(accessCode1, this.access1.value())) {
                 this.button1.hide();
                 this.access1.hide();
                 score++;
@@ -38,7 +38,7 @@ class Security {
         });
 
         this.button2.mousePressed(() => {
-            if(system.authenticate(accessCode2,this.access2.value())){
+            if (system.authenticate(accessCode2, this.access2.value())) {
                 this.button2.hide();
                 this.access2.hide();
                 score++;
@@ -46,7 +46,7 @@ class Security {
         });
 
         this.button3.mousePressed(() => {
-            if(system.authenticate(accessCode3,this.access3.value())){
+            if (system.authenticate(accessCode3, this.access3.value())) {
                 this.button3.hide();
                 this.access3.hide();
                 score++;
